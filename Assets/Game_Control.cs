57191274b81c3774ec09d8fa6cca8eefe181975a
Aujra <standishcraig@gmail.com>
2014-11-04 21:15:55 -0500

    Added Grid
    Grid is added

---
+++ Assets/Game_Control.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Control : MonoBehaviour {
+
+	public LayerMask PlanesMask;
+
+	public GameObject LastHitPlane;
+
+	public Shader Highlighted;
+	public Shader NotHighlighted;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit rayhit;
+
+		if (LastHitPlane != null) 
+		{
+			LastHitPlane.renderer.material.shader = NotHighlighted;
+		}
+
+		if (Physics.Raycast (ray, out rayhit, 1000, PlanesMask)) 
+		{
+			LastHitPlane = rayhit.collider.gameObject;
+			LastHitPlane.renderer.material.shader = Highlighted;
+		}
+
+	}
+}
